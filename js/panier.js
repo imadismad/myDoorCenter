e094a3c1c3f59bc7633101993c24896cfb56eed8
Théo KALIFA <theo.kalifa@icloud.com>
2024-03-06 16:00:10 +0100

    Add panier page

---
+++ js/panier.js
@@ -0,0 +1,40 @@
+import currentCart from "./lib/cart.js";
+import { TAXE } from "./lib/const.js";
+
+const tbody = document.getElementById("cartTable");
+
+if (currentCart.getItemInCart() === 0) {
+    currentCart.addItem(0, 10);
+    currentCart.addItem(1, 7);
+} 
+
+currentCart.forEach((item) => {
+    const line = document.createElement("tr");
+    const name = document.createElement("td");
+    const quantity = document.createElement("td");
+    const prixUnit = document.createElement("td");
+    const tva = document.createElement("td");
+    const tot = document.createElement("td");
+    const input = document.createElement("input");
+
+    input.type = "number";
+    input.value = item.getQuantity();
+    input.min = 0;
+    input.addEventListener("change", (event) => {
+        const newValue = event.target.value;
+        item.setQuantity(newValue > 0 ? newValue : 0);
+        tot.innerText = ((55.55 + 9.99) * item.getQuantity()).toFixed(2) + "€";
+        currentCart.updateSessionStorage();
+    });
+    quantity.appendChild(input);
+
+    name.textContent = item.getItemId();
+
+    prixUnit.innerText = "55.55€";
+    tva.innerText = 55.55 * TAXE + "€";
+    tot.innerText = ((55.55 + 9.99) * item.getQuantity()).toFixed(2) + "€";
+
+    line.append(name, quantity, prixUnit, tva, tot)
+
+    tbody.append(line);
+});
---
+++ panier.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="module" src="js/panier.js"></script>
+    <title>Panier</title>
+</head>
+<body>
+    <h1>Panier</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Quantité</th>
+                <th>Prix unitaire HT</th>
+                <th>TVA (20%)</th>
+                <th>Prix total TTC</th>
+            </tr>
+        </thead>
+        <tbody id="cartTable">
+
+        </tbody>
+    </table>
+
+    <label for="">Code promo : <input type="text" name="" id=""></label>
+</body>
+</html>
