ad803fbd936b1c23b029976883d98e4a2789e60a
hadjuse <rabearimananahadjpro@gmail.com>
2024-03-31 15:39:25 +0200

    inscription / deconnexion / connexion feature

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
+<?php
+session_start();
+$cookie_name = "prenom";
+setcookie($cookie_name, $_SESSION[$cookie_name], time() + 60 * 60 * 24, "/");
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Interactive PHP and HTML</title>
 </head>
+
 <body>
     <h1>Interactive PHP and HTML : Théo tu ne sers a rien :) ❤️ </h1>
-    
-    <?php
-    // Check if the form is submitted
-    session_start();
+    <p>
+        <?php
+            if (isset($_COOKIE[$cookie_name])) {
+                // Generate a personalized greeting
+                $name = $_COOKIE[$cookie_name];
+                $greeting = "Hello, " . $name . "!";
+                echo $greeting;
+            }
+        ?>
+    </p>
 
-    $cookie_name = "prenom";
-    // echo $_COOKIE[$cookie_name], $_SESSION['tel'];
-    setcookie($cookie_name, $_SESSION[$cookie_name], time() + 60*60*24,"/"); 
-    if (isset($_COOKIE[$cookie_name])) {
-        // Generate a personalized greeting
-        $greeting = "Hello, " . $_COOKIE[$cookie_name] . "!";
-        echo "<p>" . $greeting . "</p>";
-    }
-    else {
-        header("Location: connexion.html");
-    }
-    ?>
-    
+
+
     <form method="POST">
         <label for="name">Enter your name:</label>
         <input type="text" id="name" name="name" required>
         <input type="submit" value="Submit">
-        <a href="BDD/deconnexion.php">Se déconnecter</a>
     </form>
+    <a href="BDD/deconnexion.php"><button type="submit" name="submit" value="deconnexion">Déconnexion</button></a>
+    <a href="index.html">Aller à la page d'acceuil</a>
 </body>
+
 </html>
