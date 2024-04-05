e7bcfdcbf18671d1eb4a43e97628d3afdc6cfa7d
Théo KALIFA <theo.kalifa@icloud.com>
2024-04-05 20:30:15 +0200

    Adding bootstrap for the top of the page

--- panier/commande.php
+++ panier/commande.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Commande</title>
-    <link rel="stylesheet" href="/css/panier/commande.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous"
+    >
+   <link rel="stylesheet" href="/css/panier/commande.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossorigin=""/>
@@ -13,43 +18,43 @@
         crossorigin=""></script>
     <script type="module" src="/js/panier/commande.js"></script>
 </head>
-<body>
+<body class="container text-center">
     <h1>Adresse de facturation</h1>
-    <div class="info-perso">
+    <div class="text-start">
         <!-- TODO: autocomplet information base on current account -->
-        <div>
-            <label for="firstname">Prénom</label>
-            <input type="text" name="firstname-bill" id="firstname-bill">
+        <div class="form-floating mb-3">
+            <input class="form-control" type="text" id="firstname-bill" name="firstname-bill">
+            <label for="firstname-bill">Prénom</label>
         </div>
 
-        <div>
-            <label for="lastname">Nom</label>
-            <input type="text" name="lastname-bill" id="lastname-bill">
+        <div class="input-group mb-3">
+                <span class="input-group-text">Nom</span>
+                <input class="form-control" type="text" name="lastname-bill">
         </div>
 
-        <div>
-            <label for="address">Adresse</label>
-            <input type="text" name="address-bill" id="address-bill">
+        <div class="input-group mb-3">
+                <span class="input-group-text">Adresse</span>
+                <input class="form-control" type="text" name="address-bill">
         </div>
 
-        <div>
-            <label for="postal-code">Code postal</label>
-            <input type="text" name="postal-code-bill" id="postal-code-bill">
-        </div>
-        
-        <div>
-            <label for="city">Ville</label>
-            <input type="text" name="city-bill" id="city-bill">
+        <div class="input-group mb-3">
+                <span class="input-group-text">Code postal</span>
+                <input class="form-control" type="text" name="postal-code-bill">
         </div>
 
-        <div>
-            <label for="country">Pays</label>
-            <input type="text" name="country-bill" id="country-bill">
+        <div class="input-group mb-3">
+                <span class="input-group-text">Ville</span>
+                <input class="form-control" type="text" name="city-bill">
         </div>
 
-        <div>
-            <label for="phone">Numéro de portable</label>
-            <input type="text" name="phone-bill" id="phone-bill">
+        <div class="input-group mb-3">
+                <span class="input-group-text">Pays</span>
+                <input class="form-control" type="text" name="country-bill">
+        </div>
+
+        <div class="input-group mb-3">
+                <span class="input-group-text">Numéro de portable</span>
+                <input class="form-control" type="text" name="phone">
         </div>
     </div>
     <h1>Livraison</h1>
@@ -70,6 +75,10 @@
         </ul>
         <div class="point-relay" hidden>
             <div id="header-point-relay">
+                <div class="home-check-div">
+                    <label for="same-as-bill">Même information que l'adresse de facturation.</label>
+                    <input type="checkbox" id="same-as-bill" checked>
+                </div>
                 <div class="autocomplet-input">
                     <label for="pt-relay-search">Code postal</label>
                     <input type="text" id="pt-relay-search">
@@ -88,34 +97,34 @@
                 <input type="checkbox" id="same-as-bill" checked>
             </div>
 
-            <div>
-                <label for="firstname">Prénom</label>
-                <input type="text" name="firstname" id="firstname" disabled>
+            <div class="input-group mb-3">
+                    <span class="input-group-text">Prénom</span>
+                    <input class="form-control" type="text" name="firstname">
             </div>
 
-            <div>
-                <label for="lastname">Nom</label>
-                <input type="text" name="lastname" id="lastname" disabled>
+            <div class="input-group mb-3">
+                    <span class="input-group-text">Nom</span>
+                    <input class="form-control" type="text" name="lastname">
             </div>
 
-            <div>
-                <label for="address">Adresse</label>
-                <input type="text" name="address" id="address" disabled>
+            <div class="input-group mb-3">
+                    <span class="input-group-text">Adresse</span>
+                    <input class="form-control" type="text" name="address">
             </div>
 
-            <div>
-                <label for="postal-code">Code postal</label>
-                <input type="text" name="postal-code" id="postal-code" disabled>
-            </div>
-            
-            <div>
-                <label for="city">Ville</label>
-                <input type="text" name="city" id="city" disabled>
+            <div class="input-group mb-3">
+                    <span class="input-group-text">Code postal</span>
+                    <input class="form-control" type="text" name="postal-code">
             </div>
 
-            <div>
-                <label for="country">Pays</label>
-                <input type="text" name="country" id="country" disabled>
+            <div class="input-group mb-3">
+                    <span class="input-group-text">Ville</span>
+                    <input class="form-control" type="text" name="city">
+            </div>
+
+            <div class="input-group mb-3">
+                    <span class="input-group-text">Pays</span>
+                    <input class="form-control" type="text" name="country">
             </div>
         </div>
     </div>
@@ -137,5 +146,7 @@
             <input type="text" name="credit-card-cvv" id="credit-card-cvv" disabled>
         </div>
     </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
