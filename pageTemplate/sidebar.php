5ae43036d4ce3250b4216f124972085c3a459bf2
Wanbl <wanbl000.000@gmail.com>
2024-04-14 15:04:18 +0200

    sidebar design

--- pageTemplate/sidebar.php
+++ pageTemplate/sidebar.php
@@ -4,24 +4,24 @@
     </button>
     <ul class="nav flex-column">
         <li class="nav-item">
-        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#porteSubmenu">Portes</a>
-        <ul class="collapse list-unstyled" id="porteSubmenu">
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Blindées</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Intérieures</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Extérieures</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Porte-fenêtres</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Personnalisées</a></li>
-        </ul>
+            <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#porteSubmenu">Portes</a>
+            <ul class="collapse list-unstyled" id="porteSubmenu">
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Blindées</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Intérieures</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Extérieures</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Porte-fenêtres</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Personnalisées</a></li>
+            </ul>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#blocSubmenu">Blocs</a>
-        <ul class="collapse list-unstyled" id="blocSubmenu">
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Blindés</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Intérieurs</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Extérieurs</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Porte-fenêtres</a></li>
-            <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Personnalisés</a></li>
-        </ul>
+            <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#blocSubmenu">Blocs</a>
+            <ul class="collapse list-unstyled" id="blocSubmenu">
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Blindés</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Intérieurs</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Extérieurs</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Porte-fenêtres</a></li>
+                <li><a class="sidebar-subitem dropdown-item rounded border" href="#">Personnalisés</a></li>
+            </ul>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#poigneeSubmenu">Poignées</a>
