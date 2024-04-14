9f4be6d1334b9945cb8863ea5e59f37257f4c132
Wanbl <wanbl000.000@gmail.com>
2024-04-14 16:34:47 +0200

    change sidebar close conditions

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,28 +1,37 @@
-$(document).ready(function(){
-    $('#sidebarCollapse').on('shown.bs.collapse', function () {
-        $('.overlay').show();
-    });
+$(document).ready(function() {
+    var isSidebarOpen = false;
 
-    $('#sidebarCollapse').on('hidden.bs.collapse', function () {
-        $('.overlay').hide();
-    });
-});
+    $('.sidebar-small').click(function() {
 
-$(document).ready(function(){
-    $('.overlay-sidebar .close').click(function(){
-        $('#sidebarCollapse').collapse('hide');
-        $('.overlay').hide();
-    });
-});
-$(document).ready(function(){
-    $('.sidebar-small').click(function(){
         $('.sidebar-large').toggleClass('show');
         if ($('.sidebar-large').hasClass('show')) {
             $('.overlay').show();
             $('.sidebar-large').css('left', '0');
+            isSidebarOpen = true; 
+            $('#sidebarToggleButton').prop('disabled', true); 
         } else {
             $('.overlay').hide();
             $('.sidebar-large').css('left', '-200px');
+            isSidebarOpen = false;
+            $('#sidebarToggleButton').prop('disabled', false);
+        }
+    });
+
+    // Close sidebar when click on cross
+    $('.overlay-sidebar .close').click(function() {
+        $('.sidebar-large').removeClass('show').css('left', '-200px');
+        $('.overlay').hide();
+        isSidebarOpen = false;
+        $('#sidebarToggleButton').prop('disabled', false);
+    });
+
+    // Close the sidebar when click outside
+    $(document).click(function(event) {
+        if (isSidebarOpen && !$(event.target).closest('.sidebar-large, .sidebar-small').length) {
+            $('.sidebar-large').removeClass('show').css('left', '-200px');
+            $('.overlay').hide();
+            isSidebarOpen = false;
+            $('#sidebarToggleButton').prop('disabled', false);
         }
     });
 });
