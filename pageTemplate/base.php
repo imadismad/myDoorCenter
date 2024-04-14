548dbf0d438d1961e89ab12f9fdde73e217e5116
Wanbl <wanbl000.000@gmail.com>
2024-04-14 14:47:30 +0200

    add base template

---
+++ pageTemplate/base.php
@@ -0,0 +1,25 @@
+<!-- Head with automatic css imports -->
+<?php include 'pageTemplate/head.php'; ?>
+
+
+    <body>
+
+    <!-- Header import -->
+    <?php include 'pageTemplate/header.php'; ?>
+
+    <!-- Sidebar import -->
+    <?php include 'pageTemplate/sidebar.php'; ?>
+
+
+    <main class="container-fluid">
+        <!-- CONTENT HERE -->
+
+        <!-- END OF CONTENT -->
+    </main>
+
+    <!-- JS Import -->
+    <?php include 'pageTemplate/jsImport.php'; ?>
+
+    </body>
+
+<?php include 'pageTemplate/footer.php'; ?>
