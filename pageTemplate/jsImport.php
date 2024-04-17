0643ee0e7ea814e7dc99296cf54989ce18cf55f9
Wanbl <wanbl000.000@gmail.com>
2024-04-18 01:50:30 +0200

    Adapt ALL importations

--- pageTemplate/jsImport.php
+++ pageTemplate/jsImport.php
@@ -4,20 +4,39 @@
     <!-- Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Main js -->
-  <script src="js/sidebar.js"></script>
-  <script src="js/reduce-header.js"></script>
+  <?php
+  echo '<script src="'.BASE_DIR_STATIC.'js/sidebar.js"></script>';
+  echo '<script src="'.BASE_DIR_STATIC.'js/reduce-header.js"></script>';
+  ?>
 
   <!-- Get the specific js path and add it to the page -->
   <?php
     $currentPage = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 
-    $jsFilePath = "js/$currentPage";
+    $fullPath = $_SERVER['PHP_SELF'];
 
-    $scripts = scandir($jsFilePath);
+    $projectFolder = 'mydoorcenter';
+
+    $position = strpos(strtolower($fullPath), strtolower($projectFolder));
+
+    if ($position !== false) {
+        $pathBeforeFile = substr($fullPath, $position + strlen($projectFolder));
+        $directoryPath = dirname($pathBeforeFile);
+    } else {
+        $directoryPath = dirname($fullPath);
+    }
+
+    $directoryPath = trim($directoryPath, '/') . '/' . $currentPage;
+
+    $jsFilePath = BASE_DIR_STATIC . "js/" . $directoryPath;
+    $jsFilePathCheck = BASE_DIR . "js/" . $directoryPath;
+
+    $scripts = scandir($jsFilePathCheck);
 
     foreach ($scripts as $script) {
         if (pathinfo($script, PATHINFO_EXTENSION) === 'js') {
             echo '<script src="' . $jsFilePath . '/' . $script . '"></script>' . PHP_EOL;
         }
     }
-    ?>
+?>
+
