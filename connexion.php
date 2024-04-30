ebc98c2ebc65c16b9b28b6dd2a6cc6e507059cfa
Wanbl <wanbl000.000@gmail.com>
2024-04-30 22:06:51 +0200

    add constants

--- connexion.php
+++ connexion.php
@@ -1,5 +1,23 @@
 <?php
 require_once "php/Redirect.php";
+
+function getProjectPath() {
+    $path = strpos($lower = strtolower($scriptPath = $_SERVER['SCRIPT_NAME']), $projectFolder = 'mydoorcenter') !== false ?
+            substr($scriptPath, 0, strpos($lower, $projectFolder) + strlen($projectFolder)) :
+            '/';
+    return rtrim($path, '/') . '/';
+}
+
+function getAbsoluteMyDoorCenterPath() {
+  $path = __DIR__;
+  foreach (['mydoorcenter', 'wwwroot'] as $folder) {
+      if (($pos = strpos(strtolower($path), $folder)) !== false) return substr($path, 0, $pos + strlen($folder));
+  }
+  return $path;
+}
+
+define('BASE_DIR', getAbsoluteMyDoorCenterPath().'/');
+define('BASE_DIR_STATIC', getProjectPath());
 ?>
 
 <!DOCTYPE html>
