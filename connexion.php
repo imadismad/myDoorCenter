aa5fc24caf4e3d8bd5488160c4871bb36b65bc14
Wanbl <wanbl000.000@gmail.com>
2024-04-30 22:08:59 +0200

    add head import

--- connexion.php
+++ connexion.php
@@ -19,21 +19,8 @@ function getAbsoluteMyDoorCenterPath() {
 define('BASE_DIR', getAbsoluteMyDoorCenterPath().'/');
 define('BASE_DIR_STATIC', getProjectPath());
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page de connexion</title>
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <!-- Css spécifique -->
-    <link href="css/index/style.css" rel="stylesheet">
-</head>
+<!-- Head with automatic css imports -->
+<?php include BASE_DIR.'pageTemplate/head.php'; ?>
 
     <body>
 
