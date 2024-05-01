07a59d958613547c1e48622fc0ab7e88de5b0971
imadismad <i.bouaamri17@gmail.com>
2024-05-01 20:51:57 +0200

    email for client + team for contact page

---
+++ api/mailJet/mailJet.php
@@ -0,0 +1,6 @@
+<?php
+// Mailjet credentials
+define('MAILJET_API_KEY', 'defa81edb5ea0ef3317e2075e23cd9c0');
+define('MAILJET_SECRET_KEY', '5bee2c4a533e699a8cc50919ac6f20ee');
+define('MAILJET_TEMPLATE_ID', 5929442);
+?>
