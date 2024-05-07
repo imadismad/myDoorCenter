3704cbc172dfcfc0ebb2966ae71d0a9619c49b62
Wanbl <wanbl000.000@gmail.com>
2024-05-07 14:01:01 +0200

    add site map

--- map.php
+++ map.php
@@ -33,7 +33,16 @@ define('BASE_DIR_STATIC', getProjectPath());
 
     <main class="container-fluid pt-header-xs pt-header-sm pt-header-md pt-header-lg pt-header-xl">
         <!-- CONTENT HERE -->
-        
+        <h1>Plan du site</h1>
+        <ul>
+            <li><a href="<?php echo BASE_DIR_STATIC; ?>">Accueil</a></li>
+            <li><a href="<?php echo BASE_DIR_STATIC . 'connexion.php'; ?>">Connexion</a></li>
+            <li><a href="<?php echo BASE_DIR_STATIC . 'creationCompte.php'; ?>">Création de compte</a></li>
+            <li><a href="<?php echo BASE_DIR_STATIC . 'contact.php'; ?>">Assistance</a></li>
+            <li><a href="<?php echo BASE_DIR_STATIC . 'espaceClient.php'; ?>">Espace client</a></li>
+            <li><a href="<?php echo BASE_DIR_STATIC . 'legalNotice.php'; ?>">Mentions légales</a></li>
+            <li><a href="<?php echo BASE_DIR_STATIC . 'research.php'; ?>">Recherche de produits</a></li>
+        </ul>
         <!-- END OF CONTENT -->
     </main>
 
--- pageTemplate/footer.php
+++ pageTemplate/footer.php
@@ -2,7 +2,7 @@
 
   <div class="d-flex flex-grow-1 justify-content-evenly">
     <a href="<?php echo BASE_DIR_STATIC . 'contact.php'; ?>" class="link-light">Assistance</a>
-    <a href="#" class="link-light">Plan du site</a>
+    <a href="<?php echo BASE_DIR_STATIC . 'map.php'; ?>" class="link-light">Plan du site</a>
     <a href="<?php echo BASE_DIR_STATIC . 'legalNotice.php'; ?>" class="link-light">Mentions légales</a>
   </div>
   <hr>
