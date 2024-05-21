67c35746d089d7d469517e4f0a1f2a506dbdbf01
imadismad <104365795+imadismad@users.noreply.github.com>
2024-05-21 11:19:08 +0200

    Update mailJet.php

--- api/mailJet/mailJet.php
+++ api/mailJet/mailJet.php
@@ -1,7 +1,7 @@
 <?php
 // Mailjet credentials
-define('MAILJET_API_KEY', 'defa81edb5ea0ef3317e2075e23cd9c0');
-define('MAILJET_SECRET_KEY', '5bee2c4a533e699a8cc50919ac6f20ee');
+define('MAILJET_API_KEY', 'f5369a3547ea77f39ecc62c52e56e28c');
+define('MAILJET_SECRET_KEY', '1de7bc953b50d9e156c2e67dd25390a0');
 
 // Mailjet templates:
 // Template contact
